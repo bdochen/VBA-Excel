c3f8b0aba9f5700f3555aaee0cfee7b0cbd4e57e
Chen <38043557+bdochen@users.noreply.github.com>
2019-07-31 13:20:38 +0800

    Update 笔记.py

--- 笔记.py
+++ 笔记.py
@@ -1,3 +1,7 @@
 Set Values = ActiveSheet.Range("D2:D102")
 ActiveSheet.Range("D" & num)
 Range('A1:B5').ReplaceWhat:='A', Replacement:='MM', MatchCase:=True
+      
+      
+当加上On Error Resume Next语句后，如果后面的程序出现"运行时错误"时，会继续运行，不中断。
+当加上On Error Goto 0语句后，如果后面的程序出现"运行时错误"时，会显示"出错信息"并停止程序的执行。
