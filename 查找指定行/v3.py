5b10ca6aae2364e7cf2bce28d28d5baeff8e3ad0
Chen <38043557+bdochen@users.noreply.github.com>
2019-08-01 13:20:45 +0800

    Create v3.py

---
+++ 查找指定行/v3.py
@@ -0,0 +1,20 @@
+Sub test1()
+  Dim a, b, c As Range, firstAddress As String
+  On Error Resume Next
+  With Worksheets("Sheet4").Range("a1:a15")
+    Set c = .Find(2, LookIn:=xlValues)
+    If Not c Is Nothing Then
+      Set a = c
+      firstAddress = c.Address
+      Do
+        Set c = .FindNext(c)
+        If Not c Is Nothing Then
+            Set a = Union(a, c)
+        End If
+      Loop While Not c Is Nothing And c.Address <> firstAddress
+    End If
+    a.EntireRow.Select
+    a.EntireRow.Copy Destination:=Worksheets("A").Range(firstAddress)
+    'a.EntireRow.Copy Destination:=Worksheets("A").Range("A2")
+  End With
+End Sub
