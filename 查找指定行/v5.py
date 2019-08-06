435b9f2faf33f56dad83ac99d5a78e88f3e80f3d
Chen <38043557+bdochen@users.noreply.github.com>
2019-08-06 13:32:38 +0800

    Update v5.py

--- 查找指定行/v5.py
+++ 查找指定行/v5.py
@@ -1,17 +1,22 @@
 Sub 复制行中有指定数值的行()
   'LW update @ 20190801
-  Dim a, b, c As Range, firstAddress, sheetname As String, DestSheet As Worksheet
-  sheetname = "Sheet1"
-  Sheets(sheetname).Select
+  Dim a, b, c As Range, firstAddress, Sheetname, Key As String, DestSheet As Worksheet
+  
+  '定义需要取值的表名
+  Sheetname = "Sheet1"
+  '定义需要取值的字符
+  Key = 2
+  
+  Sheets(Sheetname).Select
   Set DestSheet = ActiveWorkbook.Worksheets.Add
   '产生随机表名
-  DestSheet.Name = "LW" & Int(Replace(Rnd(), ".", "") / 100)
-  
+  DestSheet.Name = "LW" & Int(Replace(Rnd(), ".", "") / 1)
+  Sheets(Sheetname).Select
   On Error Resume Next
-  With Worksheets(sheetname).Range("a1:p1")
-    Set c = .Find(2, LookIn:=xlValues, lookat:=xlWhole)
+  With Worksheets(Sheetname).Range("a1:H1")
+    Set c = .Find(Key, LookIn:=xlValues, lookat:=xlWhole)
     '下面为部分匹配
-    'Set c = .Find(2, LookIn:=xlValues, lookat:=xlPart)
+    'Set c = .Find(Key, LookIn:=xlValues, lookat:=xlPart)
     If Not c Is Nothing Then
       Set a = c
       firstAddress = c.Address
@@ -23,7 +28,7 @@ Sub 复制行中有指定数值的行()
       Loop While Not c Is Nothing And c.Address <> firstAddress
     End If
     a.EntireColumn.Select
-    a.EntireRow.Copy Destination:=DestSheet.Range(firstAddress)
+    a.EntireColumn.Copy Destination:=DestSheet.Range(firstAddress)
     'a.EntireRow.Copy Destination:=Worksheets("A").Range(firstAddress)
     'a.EntireRow.Copy Destination:=Worksheets("A").Range("A2")
   End With
